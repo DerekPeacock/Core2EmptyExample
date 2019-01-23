fe157279cefc3a36139846041872614bf6722b88
DerekPeacock <derek.peacock@yahoo.com>
2019-01-23 20:42:03 +0000

    Now contains M, V and C components

---
+++ Core2EmptyExample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Core2EmptyExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2EmptyExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(new string[] { "Robin", "Blue Tit", "Great Tit" });
+        }
+
+        public ViewResult Products ()
+        {
+            List<string> results = new List<string>();
+            foreach(Product p in Product.GetProducts())
+            {
+                string name = p?.Name;
+                decimal? price = p?.Price;
+
+                results.Add(string.Format("Name: {0}, Price: {1:c}", name, price));
+            }
+
+            return View(results);
+        }
+    }
+}
---
+++ Core2EmptyExample/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2EmptyExample.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public Nullable<decimal> Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product { Name = "Kayak", Price = 275m };
+            Product lifeJacket = new Product { Name = "Life Jacket", Price = 48.9m };
+
+            return new Product[] { kayak, lifeJacket, null };
+        }
+    }
+}
