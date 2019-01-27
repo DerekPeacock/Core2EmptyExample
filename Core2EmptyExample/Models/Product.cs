039401c40905b7dd5c75b091efb20cd388cd55e3
DerekPeacock <derek.peacock@yahoo.com>
2019-01-27 16:50:56 +0000

    Added a new repository that uses Lamda expressions

--- Core2EmptyExample/Models/Product.cs
+++ Core2EmptyExample/Models/Product.cs
@@ -8,13 +8,16 @@ namespace Core2EmptyExample.Models
     public class Product
     {
         public string Name { get; set; }
+
         public Nullable<decimal> Price { get; set; }
+
+        public string Category { get; set; } = "Water Sports";
 
         public Product RelatedProduct { get; set; }
 
         public static Product[] GetProducts()
         {
-            Product kayak = new Product { Name = "Kayak", Price = 275m };
+            Product kayak = new Product { Name = "Kayak", Category = "Water Craft", Price = 275m };
             Product lifeJacket = new Product { Name = "Life Jacket", Price = 48.9m };
 
             kayak.RelatedProduct = lifeJacket;
---
+++ Core2EmptyExample/Models/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Core2EmptyExample.Models
+{
+    public class ProductRepository
+    {
+        private static ProductRepository repository = new ProductRepository();
+
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+        public static ProductRepository SharedRepository => repository;
+
+        public ProductRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product{ Name = "Kayak", Price = 275m },
+                new Product{ Name = "Life Jacket", Price = 48.95m },
+                new Product{ Name = "Soccer Ball", Price = 19.50m },
+                new Product{ Name = "Corner Flag", Price = 34.95m },
+            };
+
+            foreach(var p in initialItems)
+            {
+                AddProduct(p);
+            }
+        }
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+    }
+}
